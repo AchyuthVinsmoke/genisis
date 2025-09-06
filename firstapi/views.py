e13bed209c3607f1536dec22f18aa3de498c3046
achyuthramreddy <achyuth.zoro@gmai.com>
2025-09-06 12:00:16 +0530

    first commit

---
+++ firstapi/views.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class Hello(APIView):
+    def get(self,request):
+        return Response({"message":"helloworld"})
